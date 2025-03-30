9ca0a2f153d55f3bb0494d1af48dc11eefedbcbf
J0wsper <bramschuijff03@gmail.com>
2025-03-30 11:44:35 -0700

    Fixed some bugs in tiebreaking and hitting. Added a new update_tiebreak method.

--- src/main.rs
+++ src/main.rs
@@ -26,8 +26,8 @@ struct Item {
 struct Cache<'a> {
     contents: BTreeMap<&'a Item, OrderedFloat<f32>>,
     policy: HitPolicy,
-    len: usize,
     size: u32,
+    occupied: u32,
 }
 
 // Wrapper for the tiebreaking order
@@ -35,14 +35,15 @@ struct Cache<'a> {
 struct Tiebreaker<'a> {
     order: VecDeque<&'a Item>,
     policy: TiebreakingPolicy,
-    len: usize,
     size: u32,
+    occupied: u32,
 }
 
 // Hit policies. The first four have a default behavior implemented. Any after that will then defer
 // the hit policy to whatever function you decide to assign to the enum. This can be anything and
 // you don't need to keep the name 'custom'.
 #[derive(Debug)]
+#[warn(dead_code)]
 enum HitPolicy {
     Lru,
     Fifo,
@@ -52,6 +53,7 @@ enum HitPolicy {
 }
 
 #[derive(Debug)]
+#[warn(dead_code)]
 enum TiebreakingPolicy {
     Lru,
     Fifo,
@@ -69,9 +71,6 @@ struct Landlord<'a> {
 // -----------------------------------------------------------------------------
 
 impl Item {
-    fn new(label: String, cost: u32, size: u32) -> Self {
-        Item { label, cost, size }
-    }
     fn get_label(&self) -> &String {
         &self.label
     }
@@ -91,16 +90,16 @@ impl<'a> Landlord<'a> {
                 Cache {
                     contents: BTreeMap::new(),
                     policy: hit_policy,
-                    len: 0,
                     size,
+                    occupied: 0,
                 }
             },
             tiebreaker: {
                 Tiebreaker {
                     order: VecDeque::new(),
                     policy: tiebreak_policy,
-                    len: 0,
                     size,
+                    occupied: 0,
                 }
             },
         }
@@ -124,6 +123,48 @@ impl<'a> Landlord<'a> {
     fn manage_tiebreak(&mut self, item: &Item) {
         let index = self.get_tiebreaker_index(item);
         self.tiebreaker.order.remove(index);
+        self.tiebreaker.occupied -= item.get_size();
+    }
+
+    // Update our tiebreaking order on any request
+    // NOTE: This is the place where custom tiebreaking is handled
+    fn update_tiebreak(&mut self, item: &'a Item) {
+        if self.tiebreaker.order.contains(&item) {
+            let index = self.get_tiebreaker_index(item);
+            match self.tiebreaker.policy {
+                // Push the item to the back of the order
+                TiebreakingPolicy::Lru => {
+                    self.tiebreaker.order.remove(index);
+                    self.tiebreaker.order.push_back(item);
+                }
+                // Do nothing
+                TiebreakingPolicy::Fifo => {}
+                // Insert the item into a random slot in the tiebreaking order
+                TiebreakingPolicy::Rand => {
+                    let k = self.tiebreaker.size;
+                    let new_index = rand::rng().random_range(0..k - 1) as usize;
+                    self.tiebreaker.order.remove(index);
+                    self.tiebreaker.order.insert(new_index, item);
+                }
+                // Perform custom tiebreaking
+                TiebreakingPolicy::Custom => self.custom_tiebreak(item),
+            }
+        } else {
+            match self.tiebreaker.policy {
+                TiebreakingPolicy::Lru => {
+                    self.tiebreaker.order.push_back(item);
+                }
+                TiebreakingPolicy::Fifo => {
+                    self.tiebreaker.order.push_back(item);
+                }
+                TiebreakingPolicy::Rand => {
+                    let k = self.tiebreaker.size;
+                    let index = rand::rng().random_range(0..k - 1) as usize;
+                    self.tiebreaker.order.insert(index, item);
+                }
+                TiebreakingPolicy::Custom => self.custom_tiebreak(item),
+            }
+        }
     }
 
     // NOTE: You need to implement this function if you want your custom hit policy to work
@@ -139,8 +180,8 @@ impl<'a> Landlord<'a> {
     }
 
     // Function called whenever Landlord hits on an item
-    // NOTE: This is where both the custom tiebreaking and hit policy are handled
-    fn hit(&mut self, label: &'a Item) -> OrderedFloat<f32> {
+    // NOTE: This is where the custom hit policy is handled
+    fn hit(&mut self, label: &'a Item) {
         let old_cred = self
             .cache
             .contents
@@ -165,32 +206,11 @@ impl<'a> Landlord<'a> {
         // Assigning our new credit to the item
         let mut _assign_cred = self.cache.contents.get(label).unwrap();
         _assign_cred = &new_cred;
-
-        // Move it around in tiebreaking order according to tiebreaking policy
-        let index = self.get_tiebreaker_index(label);
-        match self.tiebreaker.policy {
-            // Push the item to the back of the order
-            TiebreakingPolicy::Lru => {
-                self.tiebreaker.order.remove(index);
-                self.tiebreaker.order.push_back(label);
-            }
-            // Do nothing
-            TiebreakingPolicy::Fifo => {}
-            // Insert the item into a random slot in the tiebreaking order
-            TiebreakingPolicy::Rand => {
-                let k = self.tiebreaker.size;
-                let new_index = rand::rng().random_range(0..k - 1) as usize;
-                self.tiebreaker.order.remove(index);
-                self.tiebreaker.order.insert(new_index, label);
-            }
-            // Perform custom tiebreaking
-            TiebreakingPolicy::Custom => self.custom_tiebreak(label),
-        }
-        new_cred
     }
 
     // Finding the element we want to evict in the case of a tie
     fn tiebreak(&mut self, zeros: Vec<&'a Item>) -> &'a Item {
+        dbg!(&zeros);
         if zeros.len() == 1 {
             return zeros[0];
         }
@@ -208,7 +228,7 @@ impl<'a> Landlord<'a> {
         let mut pressure = OrderedFloat(0.0);
 
         // Base case
-        if self.cache.size as usize - self.cache.len >= size as usize {
+        if self.cache.size - self.cache.occupied >= size {
             return pressure;
         }
 
@@ -220,7 +240,11 @@ impl<'a> Landlord<'a> {
                 .min_by_key(|a| a.1 / OrderedFloat(a.0.get_size() as f32))
                 .expect("Could not find minimum credit element"),
         );
-        let _ = self.cache.contents.iter().map(|a| a.1 - min);
+
+        // Decrementing the credit of each item in proportion to their size
+        for (item, cred) in self.cache.contents.iter_mut() {
+            *cred -= min * item.get_size() as f32;
+        }
         pressure += min;
 
         // Finding how many items of 0 credit there are now
@@ -236,7 +260,7 @@ impl<'a> Landlord<'a> {
 
         // Removing the item it picks
         self.cache.contents.remove(evicted);
-        self.cache.len -= 1;
+        self.cache.occupied -= evicted.get_size();
 
         // Returning our pressure at the end
         pressure + self.evict(size)
@@ -245,27 +269,35 @@ impl<'a> Landlord<'a> {
     // The function called whenever the Landlord implementation faults on a request
     fn fault(&mut self, item: &'a Item) -> OrderedFloat<f32> {
         // If the cache has too many items, throw an error
-        if self.cache.len > self.cache.size as usize {
+        if self.cache.occupied > self.cache.size {
             panic!("Cache is overfull");
         }
         // If the cache has empty space, just add the item
-        else if self.cache.len < (self.cache.size + item.get_size()) as usize {
+        else if self.cache.occupied + item.get_size() <= self.cache.size {
             self.cache.contents.insert(item, item.get_cost());
-            return OrderedFloat(0.0);
+            self.cache.occupied += item.get_size();
+            self.tiebreaker.occupied += item.get_size();
+            OrderedFloat(0.0)
         }
         // Otherwise, the cache is full and we need evict something
         else {
             let size = item.get_size();
-            return self.evict(size);
+            let pressure = self.evict(size);
+            self.cache.contents.insert(item, item.get_cost());
+            pressure
         }
     }
 
     // Handle our request
     fn request(&mut self, item: &'a Item) -> OrderedFloat<f32> {
         if self.cache.contents.contains_key(&item) {
-            self.hit(item)
+            self.hit(item);
+            self.update_tiebreak(item);
+            OrderedFloat(0.0)
         } else {
-            self.fault(item)
+            let pressure = self.fault(item);
+            self.update_tiebreak(item);
+            pressure
         }
     }
 
@@ -273,6 +305,7 @@ impl<'a> Landlord<'a> {
     fn run(&mut self, trace: VecDeque<&'a Item>) {
         for request in trace.iter() {
             self.request(request);
+            dbg!(&self);
         }
     }
 }
@@ -299,6 +332,6 @@ fn main() {
     let data: &str = &fs::read_to_string("items.toml").expect("Could not read file");
     let raw_trace: TraceInfo = toml::from_str(data).expect("Could not convert TOML file");
     let item_trace = strings_to_items(&raw_trace);
-    let mut lru_landlord = Landlord::new(5, TiebreakingPolicy::Lru, HitPolicy::Lru);
+    let mut lru_landlord = Landlord::new(15, TiebreakingPolicy::Lru, HitPolicy::Lru);
     lru_landlord.run(item_trace);
 }
