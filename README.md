b10cab3df760272683ceff8ba63edc245aa26f6f
J0wsper <bramschuijff03@gmail.com>
2025-04-08 15:41:47 -0700

    Added some more comments to the code to make it easier to understand

--- README.md
+++ README.md
@@ -1,37 +1,56 @@
 # Basic Landlord Cache Simulator (csim)
-Hi! Welcome to my cache simulator. I built this for my thesis in Spring 2025 off of an existing port
-that I had made in C++ in the summer of 2024. I also expanded the functionality a little bit so that this version could read traces off of formatted TOML files instead of having to enter the trace into the command line each time.
+
+Hi! Welcome to my cache simulator. I built this for my thesis in Spring 2025
+off of an existing port that I had made in C++ in the summer of 2024. I also
+expanded the functionality a little bit so that this version could read traces
+off of formatted TOML files instead of having to enter the trace into the
+command line each time.
+
 ## How to use this
-Using this software is pretty simple: simply compile it with `cargo build`, find the executable,
-and run that by specifying the relative path to a TOML file that contains your trace. Alternatively,
-you may enter the `--no-toml` flag to enter all of these values manually. This will open an
-interactive prompt where you can enter the refresh scalar of your Landlord implementation (0 for
-FIFO-Landlord and 1 for LRU-Landlord), your cache size and your desired tie-breaking algorithm. At
-the moment, this file only contains two tie-breaking algorithms (LRU and FIFO) but you're free to
-add more! You can also enter these values directly by adding a space between your relative path to
-the TOML file and then entering them into the command line. Just enter the string corresponding to
-the tiebreaking algorithm (my parsing for the tiebreaking algorithm is case-agnostic but yours might
-not be).
+
+Using this software is pretty simple: simply compile it with `cargo build`,
+find the executable, and run that by specifying the relative path to a TOML
+file that contains your trace. Alternatively, you may enter the `--no-toml`
+flag to enter all of these values manually. This will open an interactive
+prompt where you can enter the refresh scalar of your Landlord implementation
+(0 for FIFO-Landlord and 1 for LRU-Landlord), your cache size and your desired
+tie-breaking algorithm. At the moment, this file only contains two tie-breaking
+algorithms (LRU and FIFO) but you're free to add more! You can also enter these
+values directly by adding a space between your relative path to the TOML file
+and then entering them into the command line. Just enter the string
+corresponding to the tiebreaking algorithm (my parsing for the tiebreaking
+algorithm is case-agnostic but yours might not be).
+
 ## Formatting of the TOML file
-In order for the executable to properly understand your TOML file, it needs to be formatted in a
-specific way so that `serde` can properly de-serialize it into a `trace` data structure. `serde`
-expects a few things:
-1. A table called `items`. This will contain information about each item. For instance, if you have
-   an item called `A0`, then you should have fields `A0.cost` and `A0.size` with integer values 
-   afterward.These integer values can be anything but you need to have both fields initialized or
-   else the program will produce an error. **Make sure that each item has a unique identifying
+
+In order for the executable to properly understand your TOML file, it needs to
+be formatted in a specific way so that `serde` can properly de-serialize it
+into a `trace` data structure. `serde` expects a few things:
+
+1. A table called `items`. This will contain information about each item. For
+   instance, if you have an item called `A0`, then you should have fields
+   `A0.cost` and `A0.size` with integer values afterward.These integer values can
+   be anything but you need to have both fields initialized or else the program
+   will produce an error. **Make sure that each item has a unique identifying
    string!** This is to avoid ambiguity.
-2. A table called `trace`. This will contain a numbered list of the items that you would like to
-   request from the cache in this trace. **Each of them items that are requested should be specified
-   in your items!** The executable will throw an error if it finds a request to an item not
-   specified in your items table.
-And that's it! `serde` will handle de-serializing this into a trace and the executable will run it
-with your specified Landlord variant. An example TOML file is provided with `items.toml`.
-*NOTE:* I realize that it might be kind of annoying to add items into the middle of a numbered list
-in this way if you use a text editor like VS Code. To that I say: switch to a real text editor.
-Emacs and Neovim are both great options if you're willing to learn them. In all seriousness, the
-`-no--toml` flag might be helpful for you. However, you will then have to enter your values into the
-command line which I found to be a pain in the ass.
+
+1. A table called `trace`. This will contain a numbered list of the items that
+   you would like to request from the cache in this trace. **Each of them items
+   that are requested should be specified in your items!** The executable will
+   throw an error if it finds a request to an item not specified in your items
+   table.
+
+And that's it! `serde` will handle de-serializing this into a trace and the
+executable will run it with your specified Landlord variant. An example TOML
+file is provided with `items.toml`. _NOTE:_ I realize that it might be kind of
+annoying to add items into the middle of a numbered list in this way if you use
+a text editor like VS Code. To that I say: switch to a real text editor. Emacs
+and Neovim are both great options if you're willing to learn them. In all
+seriousness, the `-no--toml` flag might be helpful for you. However, you will
+then have to enter your values into the command line which I found to be a pain
+in the ass.
+
 ## Output
-The output will be a TOML file (named `out.toml` by default) which contains trace information for
-the full trace and suffix cache. 
+
+The output will be a TOML file (named `out.toml` by default) which contains
+trace information for the full trace and suffix cache.
--- src/main.rs
+++ src/main.rs
@@ -12,19 +12,27 @@ pub mod pressure;
 // STRUCTS
 // ----------------------------------------------------------------------------
 
+// This is an enum to hold whether or not a request was a hit or a fault. This is used for both the
+// full trace cache and the suffix. The data in the fault field is the pressure increase on that
+// fault.
 #[derive(Debug)]
 pub enum RequestResult {
     Hit,
     Fault(f32),
 }
 
+// This is the data structure that serde will deserialize the items.toml file into. The items must
+// be an exhaustive list of the costs and sizes of the items requested in our trace. Meanwhile, the
+// trace is just a vector of strings where each string is an item's label.
 #[derive(Debug, Serialize, Deserialize)]
 pub struct TraceInfo {
     items: Vec<Item>,
     trace: Vec<String>,
 }
 
-// These are the keys for our cache map
+// These are the keys for our cache map. The label is the name of the item and it is also what we
+// are comparing against while iterating through our trace. Cost and size are pretty self
+// explanatory.
 #[derive(Debug, PartialOrd, PartialEq, Eq, Ord, Serialize, Deserialize)]
 pub struct Item {
     label: String,
@@ -32,7 +40,14 @@ pub struct Item {
     size: u32,
 }
 
-// Wrapper for the cache
+// Wrapper for the cache. The contents are stored as a BTreeMap where each key-value pair is an
+// item and its associated normalized credit. These credits are stored as floats to allow for
+// fractional credits. In particular, we wrap our floats in the OrderedFloat struct so that we can
+// more easily find our minimum-credit element in cache when we must evict something. This is
+// because the Ord trait is not implemented for f32 in Rust in compliance with IEEE 754. The policy
+// is our hit policy which we check in a match statement later when we have a hit. The size of our
+// cache is the number of cache lines available. Meanwhile, occupied is the number of cache lines
+// that currently have items in them.
 #[derive(Debug)]
 struct Cache<'a> {
     contents: BTreeMap<&'a Item, OrderedFloat<f32>>,
@@ -41,7 +56,11 @@ struct Cache<'a> {
     occupied: u32,
 }
 
-// Wrapper for the tiebreaking order
+// Wrapper for the tiebreaking order. This maintains a VecDeque which stores the order that, if
+// there were to be a credit tie, which order we should evict our cache items. Items closer to the
+// front will be evicted sooner, items at the back will be evicted later. Our policy is just our
+// tiebreaking policy which we match against when we must evict something. Size and occupied are
+// identical to what we had in the Cache struct.
 #[derive(Debug)]
 struct Tiebreaker<'a> {
     order: VecDeque<&'a Item>,
@@ -60,18 +79,20 @@ pub enum HitPolicy {
     Fifo,
     Rand,
     Half,
-    Custom,
 }
 
+// Tiebreaking policies. The first four have a default behavior implemented. Any after that will
+// then defer the hit policy to whatever function you decide to assign to the enum. This can be
+// anything and you don't need to keep the name 'custom'.
 #[derive(Debug)]
 #[warn(dead_code)]
 pub enum TiebreakingPolicy {
     Lru,
     Fifo,
     Rand,
-    Custom,
 }
 
+// The primary struct. We have a cache and a tiebreaker which implements our tiebreaking policy.
 #[derive(Debug)]
 pub struct Landlord<'a> {
     cache: Cache<'a>,
@@ -82,19 +103,21 @@ pub struct Landlord<'a> {
 // -----------------------------------------------------------------------------
 
 impl Item {
-    fn get_label(&self) -> &String {
+    // Getters.
+    pub fn get_label(&self) -> &String {
         &self.label
     }
-    fn get_cost(&self) -> OrderedFloat<f32> {
+    pub fn get_cost(&self) -> OrderedFloat<f32> {
         let float_cost = self.cost as f32;
         OrderedFloat(float_cost)
     }
-    fn get_size(&self) -> u32 {
+    pub fn get_size(&self) -> u32 {
         self.size
     }
 }
 
 impl<'a> Landlord<'a> {
+    // Creates a new Landlord instance with the specified size, tiebreaking policy and hit policy.
     pub fn new(size: u32, tiebreak_policy: TiebreakingPolicy, hit_policy: HitPolicy) -> Self {
         Self {
             cache: {
@@ -116,119 +139,100 @@ impl<'a> Landlord<'a> {
         }
     }
 
-    // Utility function to get the normalized credit of an item
+    // Utility function to get the normalized credit of an item as an ordered float.
     pub fn norm_credit(item: (&&'a Item, &OrderedFloat<f32>)) -> OrderedFloat<f32> {
         item.1 / OrderedFloat(item.0.get_size() as f32)
     }
 
-    // Gets the tiebreaking index of a particular item
-    fn get_tiebreaker_index(&self, item: &'a Item) -> usize {
-        self.tiebreaker
-            .order
-            .iter()
-            .position(|n| n == &item)
-            .expect("Item not found in vector")
+    // Gets the tiebreaking index of a particular item if it exists in the tiebreaking vector.
+    fn get_tiebreaker_index(&self, item: &'a Item) -> Option<usize> {
+        self.tiebreaker.order.iter().position(|n| *n == item)
     }
 
-    // Takes care of cleaning up our tiebreaking order
+    // Takes care of cleaning up our tiebreaking order by removing a particular item once it gets
+    // evicted.
     fn manage_tiebreak(&mut self, item: &Item) {
-        let index = self.get_tiebreaker_index(item);
-        self.tiebreaker.order.remove(index);
-        self.tiebreaker.occupied -= item.get_size();
-    }
-
-    // Update our tiebreaking order on any request
-    // NOTE: This is the place where custom tiebreaking is handled
-    fn update_tiebreak(&mut self, item: &'a Item) {
-        if self.tiebreaker.order.contains(&item) {
-            let index = self.get_tiebreaker_index(item);
-            match self.tiebreaker.policy {
-                // Push the item to the back of the order
-                TiebreakingPolicy::Lru => {
-                    self.tiebreaker.order.remove(index);
-                    self.tiebreaker.order.push_back(item);
-                }
-                // Do nothing
-                TiebreakingPolicy::Fifo => {}
-                // Insert the item into a random slot in the tiebreaking order
-                TiebreakingPolicy::Rand => {
-                    let k = self.tiebreaker.size;
-                    let new_index = rand::rng().random_range(0..k - 1) as usize;
-                    self.tiebreaker.order.remove(index);
-                    self.tiebreaker.order.insert(new_index, item);
-                }
-                // Perform custom tiebreaking
-                TiebreakingPolicy::Custom => self.custom_tiebreak(item),
-            }
-        } else {
-            match self.tiebreaker.policy {
-                TiebreakingPolicy::Lru => {
-                    self.tiebreaker.order.push_back(item);
-                }
-                TiebreakingPolicy::Fifo => {
-                    self.tiebreaker.order.push_back(item);
-                }
-                TiebreakingPolicy::Rand => {
-                    let k = self.tiebreaker.size;
-                    let index = rand::rng().random_range(0..k - 1) as usize;
-                    self.tiebreaker.order.insert(index, item);
-                }
-                TiebreakingPolicy::Custom => self.custom_tiebreak(item),
-            }
+        if let Some(index) = self.get_tiebreaker_index(item) {
+            self.tiebreaker.order.remove(index);
+            self.tiebreaker.occupied -= item.get_size();
         }
     }
 
-    // NOTE: You need to implement this function if you want your custom hit policy to work
-    #[warn(unused_variables)]
-    fn custom_hit(&mut self, item: &Item, old_cred: f32) -> OrderedFloat<f32> {
-        todo!()
-    }
-
-    // NOTE: You need to implement this function if you want your custom tiebreaking policy to work
-    #[warn(unused_variables)]
-    fn custom_tiebreak(&mut self, item: &Item) {
-        todo!()
+    // Update our tiebreaking order on any request.
+    fn update_tiebreak(&mut self, item: &'a Item) {
+        // Get the updated item's index
+        let index = self.get_tiebreaker_index(item);
+        // If that item is in our tiebreaking order, we remove it from the order first.
+        if let Some(loc) = index {
+            self.tiebreaker.order.remove(loc);
+        }
+        match self.tiebreaker.policy {
+            // Push the item to the back of the order
+            TiebreakingPolicy::Lru => {
+                self.tiebreaker.order.push_back(item);
+            }
+            TiebreakingPolicy::Fifo => {
+                // If the item is in cache, place it back at its original index. If it is not in
+                // cache, we push it to the back of the order.
+                match index {
+                    Some(loc) => self.tiebreaker.order.insert(loc, item),
+                    None => self.tiebreaker.order.push_back(item),
+                }
+            }
+            // Insert the item into a random slot in the tiebreaking order
+            TiebreakingPolicy::Rand => {
+                let k = self.tiebreaker.size;
+                let new_index = rand::rng().random_range(0..k - 1) as usize;
+                self.tiebreaker.order.insert(new_index, item);
+            }
+        }
     }
 
     // Function called whenever Landlord hits on an item
-    // NOTE: This is where the custom hit policy is handled
     fn hit(&mut self, label: &'a Item) {
-        let old_cred = self
-            .cache
-            .contents
-            .get(label)
-            .expect("Could not find hit item")
-            .0;
+        // We first get the item's old credit.
+        let cred = match self.cache.contents.get_mut(label) {
+            Some(val) => val,
+            None => panic!("Could not find hit item"),
+        };
 
         // Getting rid of the bad NaN case
-        if old_cred.is_nan() {
+        if cred.0.is_nan() {
             panic!("NaN credit found");
         }
 
-        // Refresh the requested item's credit according to hit policy
+        // Refresh the requested item's credit according to hit policy.
         let new_cred = match &self.cache.policy {
+            // Refreshes it to its full cost.
             HitPolicy::Lru => label.get_cost(),
-            HitPolicy::Fifo => OrderedFloat(old_cred),
-            HitPolicy::Rand => OrderedFloat(rand::rng().random_range(old_cred..label.get_cost().0)),
-            HitPolicy::Half => (label.get_cost() - old_cred) / 2.0,
-            HitPolicy::Custom => self.custom_hit(label, old_cred),
+            // Does not refresh at all.
+            HitPolicy::Fifo => *cred,
+            // Refreshes to a random value between current credit and cost.
+            HitPolicy::Rand => OrderedFloat(rand::rng().random_range(cred.0..label.get_cost().0)),
+            // Refreshes it to half its current credit.
+            HitPolicy::Half => (label.get_cost() - *cred) / 2.0,
         };
 
-        // Assigning our new credit to the item
-        let mut _assign_cred = self.cache.contents.get(label).unwrap();
-        _assign_cred = &new_cred;
+        // Assigning our new credit to the item.
+        *cred = new_cred;
     }
 
     // Finding the element we want to evict in the case of a tie
     fn tiebreak(&mut self, zeros: Vec<&'a Item>) -> &'a Item {
+        // If we only have one item that has 0 credit, we tiebreak according to that item.
         if zeros.len() == 1 {
             return zeros[0];
         }
+        // Otherwise, we iterate through our tiebreaking order from front to back, checking if each
+        // item we find is in our zeros vector. If we find a candidate in our zeros vector, then
+        // that is the element soonest on the tiebreaking order with 0 credit and so we return it.
         for cand in self.tiebreaker.order.iter() {
             if zeros.contains(cand) {
                 return cand;
             }
         }
+        // If we do not find any items in the zeros vector that are in our tiebreaking order, we
+        // have somehow mismanaged our tiebreaking order and we throw an error.
         panic!("Tiebreaking order mismanagement");
     }
 
@@ -237,7 +241,8 @@ impl<'a> Landlord<'a> {
         // Getting our return value
         let mut pressure = OrderedFloat(0.0);
 
-        // Base case
+        // Base case: we have enough space for our item and so we simply return 0
+        // because our pressure does not increase when we bring an item into cache.
         if self.cache.size - self.cache.occupied >= size {
             return pressure;
         }
@@ -255,6 +260,8 @@ impl<'a> Landlord<'a> {
         for (item, cred) in self.cache.contents.iter_mut() {
             *cred -= min * item.get_size() as f32;
         }
+        // Increasing the pressure in relation to the credit of the minimum credit item we just
+        // evicted.
         pressure += min;
 
         // Finding how many items of 0 credit there are now
@@ -268,7 +275,8 @@ impl<'a> Landlord<'a> {
         let evicted = self.tiebreak(zeros);
         self.manage_tiebreak(evicted);
 
-        // Removing the item it picks
+        // Removing the item it picks from our cache and decreasing the occupied space by the size
+        // of the item we just evicted.
         self.cache.contents.remove(evicted);
         self.cache.occupied -= evicted.get_size();
 
@@ -276,23 +284,30 @@ impl<'a> Landlord<'a> {
         pressure + self.evict(size)
     }
 
-    // The function called whenever the Landlord implementation faults on a request
+    // The function called whenever the Landlord implementation faults on a request.
     fn fault(&mut self, item: &'a Item) -> OrderedFloat<f32> {
-        // If the cache has too many items, throw an error
+        // If the cache has too many items, throw an error.
         if self.cache.occupied > self.cache.size {
             panic!("Cache is overfull");
         }
-        // If the cache has empty space, just add the item
+        // If the cache has empty space, just add the item!
         else if self.cache.occupied + item.get_size() <= self.cache.size {
+            // We insert the item into cache at full cost.
             self.cache.contents.insert(item, item.get_cost());
+            // We increase the occupied cache/tiebreaker space by our item's size.
             self.cache.occupied += item.get_size();
             self.tiebreaker.occupied += item.get_size();
+            // We return pressure 0 because we did not have to evict anything.
             OrderedFloat(0.0)
         }
         // Otherwise, the cache is full and we need evict something
         else {
+            // We get the item's size.
             let size = item.get_size();
+            // We allow our recursive eviction function to evict items until we have enough space,
+            // thereby also getting our pressure.
             let pressure = self.evict(size);
+            // We insert our item into cache at full credit.
             self.cache.contents.insert(item, item.get_cost());
             pressure
         }
@@ -300,13 +315,23 @@ impl<'a> Landlord<'a> {
 
     // Handle our request
     pub fn request(&mut self, item: &'a Item) -> RequestResult {
+        dbg!(&self);
+        // If our cache contains the requested item, we have a hit!
         if self.cache.contents.contains_key(&item) {
+            // We hit on that item, updating its credit according to hit policy.
             self.hit(item);
+            // We update our tiebreaking order.
             self.update_tiebreak(item);
+            // We return a request result of a hit
             RequestResult::Hit
-        } else {
+        }
+        // Otherwise, we have a fault :(.
+        else {
+            // We get the pressure as a result of that fault.
             let pressure = self.fault(item);
+            // We update our tiebreaking ordering no matter what.
             self.update_tiebreak(item);
+            // We wrap our pressure in a request result of a fault.
             RequestResult::Fault(*pressure)
         }
     }
@@ -320,24 +345,33 @@ impl<'a> Landlord<'a> {
         mut cost_tracker: CostTracker,
         mut pressure_tracker: PressureTracker,
     ) {
-        let pref_trace = trace.range(..suffix_start as usize);
-        let suff_trace = trace.range(suffix_start as usize..);
-        for request in trace.iter() {
+        // For each request in our trace
+        for (i, request) in trace.iter().enumerate() {
+            // We issue that request to the full trace cache because that one is going to have to
+            // service that request no matter what.
             let res = f.request(request);
+            // From there, we match on the result
             match res {
+                // If it is a hit, we log that the request was a hit with our cost logger and
+                // pressure logger.
                 RequestResult::Hit => {
                     cost_tracker.log(request, cost::RequestFullOrSuffix::Full(true));
                 }
+                // If the request was a hi, we log that the full trace cache paid that item's cost
+                // and that the pressure went up by whatever amount we wrapped in RequestResult.
                 RequestResult::Fault(pressure) => {
                     cost_tracker.log(request, cost::RequestFullOrSuffix::Full(false));
                 }
             }
-        }
-        for request in pref_trace {
-            cost_tracker.log(request, cost::RequestFullOrSuffix::Suff(true));
-        }
-        for request in suff_trace {
+            // If we are not in the suffix yet, we are going to say that S simply paid no cost.
+            // This is relevant for when we calculate individual suffix competitive ratios later.
+            if i < suffix_start as usize {
+                cost_tracker.log(request, cost::RequestFullOrSuffix::Suff(true));
+                continue;
+            }
             let res = s.request(request);
+            // We perform an identical match statement as above but instead we just label that the
+            // request results are for suff instead.
             match res {
                 RequestResult::Hit => {
                     cost_tracker.log(request, cost::RequestFullOrSuffix::Suff(true));
@@ -351,7 +385,7 @@ impl<'a> Landlord<'a> {
 }
 
 // Converts our deserialized trace of strings into a trace of items
-fn strings_to_items<'a>(trace: &'a TraceInfo) -> VecDeque<&'a Item> {
+fn strings_to_items(trace: &TraceInfo) -> VecDeque<&Item> {
     let mut requests = VecDeque::new();
     let mut counter = 0;
     for request in trace.trace.iter() {
